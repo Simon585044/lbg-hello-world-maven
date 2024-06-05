8594ea2a5c4a2d10c2619271e03a7b00b239f244
unknown <Simon@smashmail.net>
2024-06-05 11:23:54 +0100

    added hello everyone to java script

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("SIMON"));
+         System.out.println(sayHelloToSomeone("everyone"));
         System.out.println(sayGoodbye());
     }
 
